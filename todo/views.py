1ddd85d4e6989991d8a54e31c8d7d26f8916d8c0
yahomi-jp <estone111223@gmail.com>
2021-08-26 17:34:48 +0900

    hello world

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,6 @@
-from django.http import response
 from django.shortcuts import render
 
 # Create your views here.
 
 def top(request):
-    render(response, 'todo/top.html')
+    return render(request, 'todo/top.html')
