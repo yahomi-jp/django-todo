611063cc9611b99b845452ae9b281b2d819e22bf
yahomi-jp <estone111223@gmail.com>
2021-08-27 23:43:45 +0900

    editted toppage

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,13 @@
+from todo.models import Todo
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def top(request):
-    return render(request, 'todo/top.html')
+    todos = Todo.objects.all()
+    context = {
+      'todos': todos
+    }
+    return render(request, 'todo/top.html', context)
