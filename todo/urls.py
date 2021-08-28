28c8c24c94590ab577fe19c2297c7e71ec9b82c9
yahomi-jp <estone111223@gmail.com>
2021-08-28 15:12:40 +0900

    complite app -> dep

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,12 @@
 from todo import views
 from django.urls import path
 from . import views
+
+app_name = 'todo'
+
 urlpatterns = [
     path('new/',views.todo_new, name='new'),
     path('<int:todo_id>/delete/',views.todo_delete, name='delete'),
+    path('<int:todo_id>/detail/',views.todo_detail, name='detail'),
+    path('<int:todo_id>/edit/',views.todo_edit, name='edit'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,4 @@
+import todo
 from django.http.response import HttpResponseForbidden
 from todo.models import Todo
 from todo.form import TodoForm
@@ -11,8 +12,8 @@ def top(request):
     form = TodoForm
     todos = Todo.objects.all().order_by('-id')
     context = {
-      'todos': todos,
-      'form': form,
+        'todos': todos,
+        'form': form,
     }
     return render(request, 'todo/top.html', context)
 
@@ -36,3 +37,29 @@ def todo_delete(request, todo_id):
             todo.delete()
     
     return redirect(top)
+
+@login_required
+def todo_detail(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    if todo.created_by.id == request.user.id:
+        form = TodoForm(instance=todo)
+        context = {
+            'todo': todo,
+            'form': form,
+        }
+        return render(request, 'todo/detail.html', context)
+    else:
+        return HttpResponseForbidden('このTodoの閲覧は許可されていません')
+
+@login_required
+def todo_edit(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.created_by = request.user
+            todo.save()
+            return redirect(top)
+    else:
+        return HttpResponseForbidden('正規の手続きを踏んでください')
